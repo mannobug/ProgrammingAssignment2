689adc15569cab783b39f4b0f512e651d58f181f
mannobug <emmanuele.bello@gmail.com>
2015-03-18 17:35:56 +0100

    cachematrix.R
    Coursera R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Emmanuele Bello
+## this function is like a class to prevent expose variables outside 
+## using <<- operator.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+ #inverted matrix set to NULL
+  invMatr <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    invMatr <<- NULL # it also initialises invMatr to null
+  }
 
+   # get input matix
+  get <- function() x
+
+  # set & get invMatr
+  setInv <- function(inv) invMatr <<- inv 
+  getInv <- function() invMatr
+  
+  # list with basic functions
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+         mInv <- x$getInv()
+        if(!is.null(mInv)) { 
+        return(mInv) # return the calculated inversion because in result
+  }
+  
+  data <- x$get() # get mattrix object
+  
+  # using solve to calculate the inverted matrix
+  mInv <- solve(data) 
+  
+  # set calculated inverse matrix to our object
+  x$setInv(mInv) 
+  
+  # we got it!
+  mInv 
 }
